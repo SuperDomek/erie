50551acd1b3de45db2ee594fb8e86b240e6de9f8
Dominik Bláha <blahad@sic.czu.cz>
2019-01-23 15:20:13 +0100

    aktualizace phpoffice

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInit2076566b0f48e42a398cd756415b7768
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit2076566b0f48e42a398cd756415b7768::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire2076566b0f48e42a398cd756415b7768($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire2076566b0f48e42a398cd756415b7768($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
