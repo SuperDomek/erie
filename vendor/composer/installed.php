f30d36ae0e6bd07b00031aeac6cd9d6bfcd8ea54
Bláha Dominik <blaha.dominik@gmail.com>
2020-11-22 18:08:52 +0100

    composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,78 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'fdcd8eb95c2ede39350b79d967dc8f11aa0ff988',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fdcd8eb95c2ede39350b79d967dc8f11aa0ff988',
+    ),
+    'bsobbe/ithenticate' => 
+    array (
+      'pretty_version' => 'v1.0.0',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e23b12c587b74e7bbd9d287ffcc7d571dc98329c',
+    ),
+    'markbaker/complex' => 
+    array (
+      'pretty_version' => '1.5.0',
+      'version' => '1.5.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c3131244e29c08d44fefb49e0dd35021e9e39dd2',
+    ),
+    'markbaker/matrix' => 
+    array (
+      'pretty_version' => '1.2.1',
+      'version' => '1.2.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '182d44c3b2e3b063468f7481ae3ef71c69dc1409',
+    ),
+    'phpoffice/phpspreadsheet' => 
+    array (
+      'pretty_version' => '1.8.2',
+      'version' => '1.8.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0c1346a1956347590b7db09533966307d20cb7cc',
+    ),
+    'phpxmlrpc/phpxmlrpc' => 
+    array (
+      'pretty_version' => '4.0.0',
+      'version' => '4.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '679eacd661962f353a809ab66e83a86233f28bcc',
+    ),
+    'psr/simple-cache' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '408d5eafb83c57f6365a3ca330ff23aa4a5fa39b',
+    ),
+  ),
+);
